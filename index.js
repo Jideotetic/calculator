e843e4f96fec69a9170218f8844463cecdeba7ff
Jideotetic <jideotetic@gmail.com>
2023-07-29 01:56:18 +0100

    Implemented full function for additin

--- index.js
+++ index.js
@@ -1,261 +1,205 @@
+'use strict';
+const buttons = document.querySelector('.buttons-container').children;
 const output = document.querySelector('.output');
-const result = document.querySelector('.result');
+const input = document.querySelector('.input');
 let buffer = '0';
-let initialVal = 0;
-let previousOperator;
-const buttonsTexts = [
-  '%',
-  'ON',
-  'C',
-  'DEL',
-  '1/x',
-  'x²',
-  '√x',
-  '/',
-  '7',
-  '8',
-  '9',
-  'X',
-  '4',
-  '5',
-  '6',
-  '-',
-  '1',
-  '2',
-  '3',
-  '+',
-  '+/-',
-  '0',
-  '.',
-  '=',
-];
-function renderButtons(container) {
-  for (let i = 0; i < 24; i++) {
-    const button = document.createElement('button');
-    container.appendChild(button);
-    let buttonText = buttonsTexts.shift();
-    button.textContent = buttonText;
-    if (i === 1 || i === 2 || i === 3) {
-      button.classList.add('clear');
-    } else if (
-      i === 0 ||
-      i === 4 ||
-      i === 5 ||
-      i === 6 ||
-      i === 7 ||
-      i === 11 ||
-      i === 15 ||
-      i === 19 ||
-      i === 20 ||
-      i === 23
-    ) {
-      button.classList.add('operator');
-    } else if (
-      i === 8 ||
-      i === 9 ||
-      i === 10 ||
-      i === 12 ||
-      i === 13 ||
-      i === 14 ||
-      i === 16 ||
-      i === 17 ||
-      i === 18 ||
-      i === 21
-    ) {
-      button.classList.add('number');
-    } else if (i === 22) {
-      button.classList.add('point');
-    }
-  }
+let initialValue = 0;
+let finalValue = 0;
+let operator = null;
+let calculating = true;
+
+for (const button of buttons) {
+  button.addEventListener('click', handleButton);
+
+  button.addEventListener('transitionend', (e) => {
+    e.target.classList.remove('clicked');
+  });
 }
 
-// function handleUranary(value) {
-//   if (buffer === undefined) {
-//     return;
-//   }
-
-//   if (initialVal === 0) {
-//     initialVal = Number(buffer);
-//   } else {
-//     initialVal = initialVal;
-//   }
-
-//   if (buffer === '0' && value === '1/x' && initialVal === 0) {
-//     output.textContent = 'Cannot divide by zero';
-//   } else if (value === '1/x') {
-//     initialVal = 1 / initialVal;
-//     output.textContent = initialVal;
-//   } else if (value === 'x²') {
-//     initialVal = initialVal ** 2;
-//     output.textContent = initialVal;
-//   } else if (value === '√x') {
-//     initialVal = Math.sqrt(initialVal);
-//     output.textContent = initialVal;
-//   }
-
-// }
-
-// function handleMath(value) {
-//   if (buffer === undefined) {
-//     return;
-//   }
-
-//   let intBuffer = Number(buffer);
-//   if (initialVal === 0) {
-//     initialVal = intBuffer;
-//   } else {
-//     flushOperation(intBuffer);
-//   }
-
-//   previousOperator = value;
-//   buffer = '0';
-//   output.textContent = initialVal;
-// }
-
-// function flushOperation(intBuffer) {
-//   if (previousOperator === '+') {
-//     initialVal += intBuffer;
-//   } else if (previousOperator === '-') {
-//     initialVal -= intBuffer;
-//   } else if (previousOperator === 'X') {
-//     initialVal *= intBuffer;
-//   } else if (previousOperator === '/') {
-//     initialVal /= intBuffer;
-//   }
-// }
-
-function powerOnAndOff(value) {
-  if (value === 'ON') {
-    buffer = '0';
-    output.textContent = buffer;
-    document.querySelector('.clear').textContent = 'OFF';
-  } else if (value === 'OFF') {
-    buffer = '0';
-    output.textContent = '';
-    document.querySelector('.clear').textContent = 'ON';
-  }
-}
-
-function handlepoint(value) {
-  if (output.textContent === '') {
-    return;
-  }
-  if (buffer === '0') {
-    buffer = `${buffer}${value}`;
-    output.textContent = buffer;
-  } else if (buffer.includes('.')) {
-    return;
+function handleButton(e) {
+  debugger;
+  e.target.classList.add('clicked');
+  if (isNaN(e.target.textContent)) {
+    handleSymbol(e.target.textContent);
   } else {
-    buffer += value;
-    output.textContent = buffer;
-  }
-}
-
-function handleClear(value) {
-  if (output.textContent === '') {
-    return;
-  }
-  if (value === 'C') {
-    buffer = '0';
-    output.textContent = buffer;
-  } else if (value === 'DEL') {
-    if (buffer.length === 1) {
-      buffer = '0';
-      output.textContent = buffer;
-    } else {
-      buffer = buffer.slice(0, -1);
-      output.textContent = buffer;
-    }
-  }
-}
-
-function handleSpecialOperator(value) {
-  if (output.textContent === '') {
-    return;
-  }
-  if (value === '1/x') {
-    if (buffer === '0') {
-      output.textContent = 'Cannot divide by zero';
-    } else {
-      buffer = 1 / Number(buffer);
-      buffer = buffer.toString();
-      output.textContent = buffer;
-    }
-  } else if (value === 'x²') {
-    buffer = Number(buffer) ** 2;
-    buffer = buffer.toString();
-    output.textContent = buffer;
-  } else if (value === '√x') {
-    buffer = Math.sqrt(Number(buffer));
-    buffer = buffer.toString();
-    output.textContent = buffer;
-  }
-}
-
-function flushOperation(intBuffer) {
-  if (previousOperator === '+') {
-    initialVal += intBuffer;
-    result.textContent = `(${initialVal})`;
-  } else if (previousOperator === '-') {
-    initialVal -= intBuffer;
-    result.textContent = `(${initialVal})`;
-  } else if (previousOperator === 'X') {
-    initialVal *= intBuffer;
-    result.textContent = `(${initialVal})`;
-  } else if (previousOperator === '/') {
-    initialVal /= intBuffer;
-    result.textContent = `(${initialVal})`;
-  }
-}
-
-function handleMath(value) {
-  if (output.textContent === '') {
-    return;
-  }
-  if (initialVal === 0) {
-    initialVal = Number(buffer);
-  } else {
-    flushOperation(Number(buffer));
+    handleNumber(e.target.textContent);
   }
 
-  previousOperator = value;
-  buffer = '0';
+  console.log(
+    'buffer',
+    '==>',
+    buffer,
+    'initialValue',
+    '==>',
+    initialValue,
+    'operator',
+    '==>',
+    operator,
+    'calculating',
+    '==>',
+    calculating,
+    'finalValue',
+    '==>',
+    finalValue
+  );
 }
 
 function handleSymbol(value) {
   switch (value) {
     case 'ON':
     case 'OFF':
-      powerOnAndOff(value);
-      break;
-    case '.':
-      handlepoint(value);
+      onOffCalculator(value);
       break;
     case 'C':
     case 'DEL':
-      handleClear(value);
+      deleteOutput(value);
       break;
-    case '1/x':
-    case 'x²':
-    case '√x':
-      handleSpecialOperator(value);
+    case '.':
+      handlePoint(value);
       break;
-    case '/':
-    case 'X':
-    case '-':
     case '+':
+    case '-':
+    case '÷':
+    case 'x':
       handleMath(value);
       break;
     case '=':
-      if (previousOperator === null) {
-        return;
-      } else { 
-        flushOperation(Number(buffer));
-        previousOperator = null;
-        buffer = initialVal;
-        initialVal = 0;
+      flushOperation(+buffer);
+      break;
+    default:
+  }
+}
+
+function onOffCalculator(value) {
+  if (value === 'ON') {
+    buffer = '0';
+    initialValue = 0;
+    finalValue = 0;
+    operator = null;
+    calculating = true;
+    output.textContent = buffer;
+    document.querySelector('.clear').textContent = 'OFF';
+  } else {
+    buffer = '0';
+    initialValue = 0;
+    finalValue = 0;
+    operator = null;
+    calculating = true;
+    output.textContent = '';
+    input.textContent = '';
+    document.querySelector('.clear').textContent = 'ON';
+  }
+}
+
+function deleteOutput(value) {
+  if (output.textContent === '') {
+    return;
+  }
+
+  if (value === 'C') {
+    buffer = '0';
+    initialValue = 0;
+    finalValue = 0;
+    operator = null;
+    calculating = true;
+    output.textContent = buffer;
+    input.textContent = '';
+  } else if (value === 'DEL') {
+    if (initialValue !== 0) {
+      input.textContent = '';
+    } else if (calculating) {
+      return;
+    } else if (!calculating) {
+      if (buffer.length === 1) {
+        buffer = '0';
+        output.textContent = buffer;
+      } else {
+        buffer = buffer.slice(0, -1);
         output.textContent = buffer;
       }
+    }
+  }
+}
+
+function handlePoint(value) {
+  if (output.textContent === '') {
+    return;
+  }
+
+  if (buffer.includes(value)) {
+    return;
+  } else {
+    buffer += value;
+    output.textContent = buffer;
+  }
+
+  calculating = false;
+}
+
+function handleMath(value) {
+  if (output.textContent === '') {
+    return;
+  }
+
+  if (buffer.indexOf('.') === buffer.length - 1) {
+    buffer = buffer.slice(0, -1);
+  }
+
+  if (buffer.length === 3 && buffer.includes('.') && buffer.slice(-1) === '0') {
+    buffer = buffer.slice(0, -2);
+  }
+
+  if (initialValue === 0) {
+    initialValue = +buffer;
+    finalValue = +buffer;
+    output.textContent = buffer;
+    input.textContent = `${initialValue}${value}`;
+  } else if (calculating) {
+    output.textContent = `${initialValue}`;
+  } else {
+    flushOperation(+buffer);
+  }
+
+  input.textContent = `${initialValue}${value}`;
+  operator = value;
+  buffer = '0';
+  calculating = true;
+  finalValue = initialValue;
+}
+
+function flushOperation(value) {
+  if (operator === '+') {
+    if (!calculating) {
+      finalValue = value;
+    }
+    if (input.textContent.includes('=')) {
+      input.textContent = `${initialValue}${operator}${finalValue}=`;
+      if (!Number.isInteger(initialValue) && !Number.isInteger(finalValue)) {
+        initialValue = (initialValue * 10 + finalValue * 10) / 10;
+      } else {
+        initialValue += finalValue;
+      }
+    } else {
+      input.textContent += `${finalValue}=`;
+      if (!Number.isInteger(initialValue) && !Number.isInteger(finalValue)) {
+        initialValue = (initialValue * 10 + finalValue * 10) / 10;
+      } else {
+        initialValue += finalValue;
+      }
+    }
+    output.textContent = `${initialValue}`;
+  } else if (operator === '-') {
+    initialValue -= value;
+    output.textContent = `${initialValue}`;
+    input.textContent += `${value}=`;
+  } else if (operator === 'x') {
+    initialValue *= value;
+    output.textContent = `${initialValue}`;
+    input.textContent += `${value}=`;
+  } else if (operator === '÷') {
+    initialValue /= value;
+    output.textContent = `${initialValue}`;
+    input.textContent += `${value}=`;
   }
 }
 
@@ -263,6 +207,7 @@ function handleNumber(value) {
   if (output.textContent === '') {
     return;
   }
+
   if (buffer === '0') {
     buffer = value;
     output.textContent = buffer;
@@ -270,97 +215,6 @@ function handleNumber(value) {
     buffer += value;
     output.textContent = buffer;
   }
+
+  calculating = false;
 }
-
-function buttonClick(value) {
-  if (isNaN(value)) {
-    handleSymbol(value);
-    console.log(value, buffer);
-  } else {
-    handleNumber(value);
-    console.log(value, buffer);
-  }
-}
-
-function handleButton(e) {
-  return buttonClick(e.target.textContent);
-}
-
-function init() {
-  const buttonContainer = document.querySelector('.buttons-container');
-  renderButtons(buttonContainer);
-  buttonContainer.addEventListener('click', handleButton);
-}
-
-init();
-
-// buttonsContainer.addEventListener('click', (e) => {
-//   if (e.target.textContent === 'ON') {
-//     output.textContent = buffer;
-//     e.target.textContent = 'OFF';
-//   } else if (e.target.textContent === 'OFF') {
-//     output.textContent = '';
-//     e.target.textContent = 'ON';
-//   } else if (e.target.textContent === 'C' && output.textContent !== '') {
-//     output.textContent = '0';
-//   } else if (
-//     e.target.textContent === 'DEL' &&
-//     (output.textContent === '0' || output.textContent.length === 1)
-//   ) {
-//     output.textContent = '0';
-//   } else if (e.target.textContent === 'DEL' && output.textContent.length > 1) {
-//     output.textContent = output.textContent.slice(0, -1);
-//   } else if (e.target.textContent === '1/x') {
-//     if (output.textContent === '0') {
-//       output.textContent = 'Cannot divide by zero';
-//     } else {
-//       output.textContent = 1 / Number(output.textContent);
-//     }
-//   } else if (e.target.textContent === 'x²') {
-//     output.textContent = Number(output.textContent.replaceAll(',', '')) ** 2;
-//   } else if (e.target.textContent === '√x') {
-//     output.textContent = Math.sqrt(
-//       Number(output.textContent.replaceAll(',', ''))
-//     );
-//   } else if (e.target.textContent === '+/-') {
-// output.textContent = -Number(output.textContent);
-//   } else if (
-//     e.target.textContent === '0' ||
-//     e.target.textContent === '1' ||
-//     e.target.textContent === '2' ||
-//     e.target.textContent === '3' ||
-//     e.target.textContent === '4' ||
-//     e.target.textContent === '5' ||
-//     e.target.textContent === '6' ||
-//     e.target.textContent === '7' ||
-//     e.target.textContent === '8' ||
-//     e.target.textContent === '9'
-//   ) {
-//     if (output.textContent === '') {
-//       return;
-//     } else if (output.textContent === '0') {
-//       output.textContent = e.target.textContent;
-//     } else {
-//       output.textContent += e.target.textContent;
-//     }
-//   } else if (e.target.textContent === '/') {
-//     sum = output.textContent;
-//   }
-
-//   // console.log(e.target.textContent, output.textContent);
-// });
-
-// function addComma() {
-//   // let j = -1;
-//   let array = [];
-//   let length = output.textContent.length;
-//   for (let i = length; i > 0; i--) {
-//     let outputArray = output.textContent.split('');
-//     console.log('outputArray', outputArray);
-//     array.unshift(outputArray[i]);
-//     console.log('array', array);
-
-//     // outputArray.splice(-i, 0, ',');
-//     // output.textContent = outputArray.join('');
-//   }
-// }
